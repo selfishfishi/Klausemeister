86f33e2a02ddddd7015d94b53598590a4ff22c34
Ali Fathalian <a.fathalian87@gmail.com>
2026-04-04 08:52:52 -0700

    Initial Commit

---
+++ Klausemeister/KlausemeisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlausemeisterApp.swift
+//  Klausemeister
+//
+//  Created by Ali Fathalian on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct KlausemeisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
